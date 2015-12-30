7155eca814a94a505b6ba07835df16145a5a60ce
lenuo <lenuo@sina.com>
2015-12-30 08:37:54 +0800

    cesh

--- GitTestProject/Program.cs
+++ GitTestProject/Program.cs
@@ -17,6 +17,8 @@ namespace GitTestProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+         //   w
         }
     }
 }
